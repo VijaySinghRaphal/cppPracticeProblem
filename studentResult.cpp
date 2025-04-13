243ad17063095ae1c95eb7b023983792fa4bdd97
vijay singh <vijaysinghraphal@gmail.com>
2025-04-13 21:56:54 +0530

    add studentResult.cpp

---
+++ studentResult.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int id;
+string name;
+float physics,chemistry,maths,english,hindi;
+float total,percentage;
+
+cout<<"Enter student id ";
+cin>>id;
+cout<<"Enter student (first) name ";
+cin>>name;
+cout<<"Enter physics marks ";
+cin>>physics;
+cout<<"Enter chemistry marks ";
+cin>>chemistry;
+cout<<"Enter maths marks ";
+cin>>maths;
+cout<<"Enter english marks ";
+cin>>english;
+cout<<"Enter hindi marks ";
+cin>>hindi;
+
+if ((physics < 0 || physics > 100) ||
+        (chemistry < 0 || chemistry > 100) ||
+        (maths < 0 || maths > 100) ||
+        (english < 0 || english > 100) ||
+        (hindi < 0 || hindi > 100)) {
+        cout << "Invalid marks. Please enter marks between 0 and 100." << endl;
+        return 0;
+    }
+
+if(physics >= 40 && chemistry >= 40 && maths >= 40 && english >= 40 && hindi >=40){
+    total = physics + chemistry + maths + english + hindi;
+    percentage = total / 5;
+
+    string result;
+    if(percentage >= 75 && percentage <= 100){
+        result = "Excellent";
+    }
+    else if(percentage >= 60 && percentage < 75){
+        result = "First class";
+    }
+    else if(percentage >= 40 && percentage < 60){
+        result = "Pass";
+    }
+    else if(percentage >= 0 && percentage < 40){
+        result = "Fail";
+    }
+    else{
+        result = "Invalid Result";
+    }
+
+    cout<<"ID\tNAME\tPHYSICS\tCHEMSITRY\tMATHS\tENGLISH\tHINDI\tTOTAL\tPERCENTAGE\tRESULT"<<endl;
+    cout<<id<<"\t"<<name<<"\t"<<physics<<"\t"<<chemistry<<"\t"<<maths<<"\t"<<english<<"\t"<<hindi<<"\t"<<total<<"\t"<<percentage<<endl;
+}
+else{
+    cout << "Result: Fail (Student failed in one or more subjects)" << endl;
+}
+return 0;
+}
