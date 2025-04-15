ab0b7253e08cdb07f84ce19257bba7fbc1389335
vijay singh <vijaysinghraphal@gmail.com>
2025-04-15 22:33:31 +0530

    updated countdigit.cpp

--- 7countDigit.cpp
+++ 7countDigit.cpp
@@ -11,25 +11,6 @@ Features:
 - Includes handling for the edge case where the input is zero (which has 1 digit).
 */
 
-#include<iostream>
-using namespace std;
-
-int main(){
-    // Declare variables: 'num' to store the input number and 'c' to count the number of digits
-    int num, c = 0;
-
-    // Prompt the user to enter a number
-    cout << "Enter the number: ";
-    cin >> num;
-
-    // Check if the number is zero (since zero has 1 digit)
-    if (num == 0) {
-        c = 1;  // If the input number is 0, we know it has 1 digit
-    } else {
-        // Loop to count the number of digits in the input number
-        while(num > 0){
-            c++;        // Increment the counter for each digit
-            num = num / 10;
 
 #include<iostream>
 using namespace std;
